afbf74fea15af35ceb81e791fce030945ba57cf5
Sunny Cletus <scletus@gmail.com>
2020-07-16 21:44:45 +0100

    pre-rendering added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
-import AuthProvider from './context/authContext'
-import App from './App';
-import './index.css'
+import React from "react";
+// import { render } from 'react-dom';
+import { HashRouter as Router } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import AuthProvider from "./context/authContext";
+import App from "./App";
+import { hydrate, render } from "react-dom";
+import "./index.css";
 
-
-
-render(
+/*render(
   <AuthProvider>
     <Router hashType='noslash'>
       <App />
     </Router>
   </AuthProvider>,
   document.getElementById('root')
-);
+);*/
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <AuthProvider>
+      <Router hashType="noslash">
+        <App />
+      </Router>
+    </AuthProvider>,
+    rootElement
+  );
+} else {
+  render(
+    <AuthProvider>
+      <Router hashType="noslash">
+        <App />
+      </Router>
+    </AuthProvider>,
+    rootElement
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
